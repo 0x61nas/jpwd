d79140c4015a2c855e7b30bc2246c19aed9cba3c
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-06-13 20:08:59 +0200

    Create the main class ▶

---
+++ src/main/java/com/anas/javautils/jpwd/CurrentDirectoryInfo.java
@@ -0,0 +1,6 @@
+package com.anas.javautils.jpwd;
+
+public class CurrentDirectoryInfo {
+    private ColoredString separator;
+    private ColoredString[] dirs;
+}
---
+++ src/main/java/com/anas/javautils/jpwd/Main.java
@@ -0,0 +1,11 @@
+package com.anas.javautils.jpwd;
+
+import com.anas.javautils.jpwd.args.ArgumentProcessor;
+
+public class Main {
+    public static final String VERSION = "0.0.1";
+    public static void main(final String[] args) {
+        ArgumentProcessor.getInstance().process(args); // Process the arguments
+       System.out.println(new CurrentDirectoryInfo().toString());
+    }
+}
