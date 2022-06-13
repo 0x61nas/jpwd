eaa1aec229da81a545204b1668d8b6d9f602c7aa
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-06-13 19:05:21 +0200

    Add options list 😆

---
+++ src/main/java/com/anas/javautils/jpwd/args/CLIOption.java
@@ -0,0 +1,33 @@
+package com.anas.javautils.jpwd.args;
+
+import org.apache.commons.cli.Option;
+
+public enum CLIOption {
+    HELP("h", "help", false, "Print this message"),
+    VERSION("v", "version", false, "Print version information"),
+    SEPARATOR("s", "separator", true, """
+    Set the separator (like '-'),
+    default is system separator (like '\\' in windows or '/' in linux and unix like systems)"""),
+    SEPARATOR_COLOR("sc", "separator-color", true, """
+    Set the separator color, as a hexadecimal only like '#ff0000'"""),
+    NO_COLORS("nc", "no-colors", false, "Disable colors"),
+    ALIGNMENT("a", "alignment", true, """ 
+            Set the output alignment (left, right, center), default is left"""),
+    BACKGROUND("b", "background", true, """
+    Set the background color default is none, as a hexadecimal only like '#ff0000'"""),
+    NO_ICONS("ni", "no-icons", false, "Disable icons"),
+    ;
+
+    private final Option option;
+
+    CLIOption(final String shortName,
+              final String longName,
+              final boolean hasArgument,
+              final String description) {
+        option = new Option(shortName, longName, hasArgument, description);
+    }
+
+    public Option getOption() {
+        return option;
+    }
+}
